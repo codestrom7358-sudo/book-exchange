1707354cfb0c7170d3fb145927de8f4cdf9919e7
codestrom7358-sudo <codestrom7358@gmail.com>
2025-12-16 14:17:39 +0530

    login and register page

--- src/main/java/com/todo/bookexchange/controller/UserController.java
+++ src/main/java/com/todo/bookexchange/controller/UserController.java
@@ -5,18 +5,42 @@ import com.todo.bookexchange.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-@CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/auth")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
+    // REGISTER NEW USER
+    @PostMapping("/register")
+    public String register(@RequestBody User user) {
+
+        User existing = userRepository.findByPhone(user.getPhone());
+
+        if (existing != null) {
+            return "Phone already registered";
+        }
+
+        userRepository.save(user);
+        return "Registration successful";
+    }
+
+    // LOGIN OLD USER
     @PostMapping("/login")
-    public User login(@RequestBody User user){
-        Optional<User> exist = userRepository.findByMobile(user.getMobile());
-        return exist.orElseGet(() -> userRepository.save(user));
+    public String login(@RequestBody User user) {
+
+        User existing = userRepository.findByPhone(user.getPhone());
+
+        if (existing == null) {
+            return "User not found";
+        }
+
+        if (existing.getPassword().equals(user.getPassword())) {
+            return "Login successful";
+        } else {
+            return "Wrong password";
+        }
     }
 }
